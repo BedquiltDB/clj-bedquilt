5fb4b62bd1068558588d50cc6f1f648cbf40982e
Shane Kilkelly <shane@kilkelly.me>
2015-12-12 12:08:04 +0000

    Change how the connection is built

--- src/bedquilt/core.clj
+++ src/bedquilt/core.clj
@@ -2,6 +2,18 @@
   (:require [clojure.java.jdbc :as j]))
 
 
+
+(defn- build-jdbc-connection [spec]
+  (j/get-connection (assoc spec :subprotocol "postgresql")))
+
+
 (defn get-connection [config]
-  (let [connection (j/get-connection (assoc config :subprotocol "postgresql"))]
-    {:jdbc-conn connection}))
+  (let [host (or (:host config) "127.0.0.1")
+        port (or (:port config) "5432")
+        db   (or (:db   config) "")
+        subname (str "//" host ":" port "/" db)
+        spec (-> config
+                 (dissoc :host :port :db)
+                 (assoc :subname subname))
+        jdbc-conn (build-jdbc-connection spec)]
+    {:jdbc-conn jdbc-conn}))
