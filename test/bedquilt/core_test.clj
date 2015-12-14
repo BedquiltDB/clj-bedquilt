2ffd7ea92590f4bbd2dd409211173826e776919a
Shane Kilkelly <shane@kilkelly.me>
2015-12-14 21:05:32 +0000

    more

--- test/bedquilt/core_test.clj
+++ test/bedquilt/core_test.clj
@@ -3,10 +3,7 @@
             [bedquilt.core :as bq]
             [bedquilt.test-utils :refer [get-test-connection reset-db!]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
-
+(def spec (get-test-connection))
 
 (deftest basics
   (testing "connection"
@@ -16,16 +13,14 @@
 (deftest list-collections
 
   (testing "with no collections"
-    (let [spec (get-test-connection)
-          _ (reset-db! spec)
+    (let [_ (reset-db! spec)
           collections (bq/list-collections spec)]
       (is (empty? collections))))
 
   (testing "after creating two collections"
-    (let [spec (get-test-connection)]
-      (do
-        (reset-db! spec)
-        (is (= true (bq/create-collection spec "coll_one")))
-        (is (= true (bq/create-collection spec "coll_two")))
-        (is (= '("coll_one" "coll_two")
-               (bq/list-collections spec)))))))
+    (do
+      (reset-db! spec)
+      (is (= true (bq/create-collection spec "coll_one")))
+      (is (= true (bq/create-collection spec "coll_two")))
+      (is (= '("coll_one" "coll_two")
+             (bq/list-collections spec))))))
