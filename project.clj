ae4513624b1eed2c8d4d3b31cf7d64834ab23aa8
Shane Kilkelly <shane@kilkelly.me>
2016-01-06 18:07:00 +0000

    Add autogenerated docs.

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject bedquilt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject bedquilt "0.1.0"
+  :description "Clojure driver for BedquiltDB"
+  :url "http://github.com/BedquiltDB/clj-bedquilt"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.4.2"]
                  [cheshire "5.5.0"]
                  [org.postgresql/postgresql "9.4-1206-jdbc42"]]
-  :profiles {:test {}})
+  :profiles {:test {}}
+  :plugins [[lein-codox "0.9.1"]]
+  :codox {:namespaces [bedquilt.core]
+          :output-path "html-docs"})
